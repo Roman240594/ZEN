ce2333a678f48dbfef1599ca1d67560fdf2f9513
Roman Linevych <linevyhroman@gmail.com>
2021-08-25 14:56:21 +0300

    create home page

---
+++ app/js/main.js
@@ -0,0 +1,5 @@
+const footerMenu = document.querySelectorAll('.footer__menu-title')
+
+const openMenu = accordion => {
+    const menuContent => accordion.('.footer__menu')
+}
---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const { src, dest, parallel, series, watch } = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const prefix = require('gulp-autoprefixer');
+
+function browser() {
+    browserSync.init({
+        server: {
+            baseDir: './app/'
+        },
+        notify: false
+    });
+}
+
+function watchFiles() {
+    watch("app/scss/**/*.scss", css);
+    watch("app/*.html").on('change', browserSync.reload);
+}
+
+function css() {
+    return src("app/scss/**/*.scss")
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: false }))
+        .pipe(dest("app/css"))
+        .pipe(browserSync.stream());
+}
+
+exports.css = css;
+exports.default = series(series(css), parallel(browser, watchFiles));
